c0914d501858b22942f19a5fd19b768fe9c6e2ef
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2020-11-04 12:32:17 -0300

    Implementado a lib do aos para fazer o efeito de transicao entre as paginas.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ScaryPage from './pages/home-scary/scary';
 
 import './styles/reset.css';
 
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 function App() {
+  useEffect(()=> {
+    Aos.init({duration:3000})
+  }, [])
   return (
-    <ScaryPage props="/" />
+    <>
+      <ScaryPage  data-aos="fade-up" props="/" />
+      <ScaryPage data-aos="fade-up" props="/" />
+      <ScaryPage data-aos="fade-up" props="/" />
+    </>
   );
 }
 
--- src/pages/home-scary/scary.tsx
+++ src/pages/home-scary/scary.tsx
@@ -1,15 +1,17 @@
 //Imports from react
-import React from 'react';
+import React, {useEffect} from 'react';
+import Aos from 'aos';
 
 //Imports from styled components
 import  { ScaryPageTag} from './styled-scary';
+import 'aos/dist/aos.css';
 
 //Imports de components
 import HeaderComponent from '../../components/header/header';
 import Footer from '../../components/footer/footer';
 
 //Imports de Images
-import MainSkull from '../../images/SkullMain.svg'
+import MainSkull from '../../images/SkullMain.svg';
 import Marker from '../../assets/mark.svg';
 import Arrow from '../../assets/arrow.svg';
 import stepper from '../../assets/stepper.svg';
@@ -20,11 +22,13 @@ interface Param{
 }
 
 const ScaryPage:React.FC<Param> = props=>{
-
+    useEffect(()=> {
+        Aos.init({duration: 2000})
+    }, [])
     return(
             <ScaryPageTag>
                 <HeaderComponent props="/"/>
-                <main id="Container">
+                <main data-aos="slide-left" id="Container">
                     <div className="Image">
                        <img className="Image" src={MainSkull} alt="" />
                        <div className="AboutSkull">
@@ -36,7 +40,7 @@ const ScaryPage:React.FC<Param> = props=>{
                     <div className="Text">
                         <div>
                             <h3 className="Texto">#1 TOP SCARIEST GHOST 2020</h3>
-                            <h1 className="Texto">RESPAWN</h1>
+                            <h1 className="Texto" >RESPAWN</h1>
                             <h1 className="Texto">THE SPOOKY</h1>
                             <h1 className="Texto">SKULL</h1>
                             <p className="Texto">
